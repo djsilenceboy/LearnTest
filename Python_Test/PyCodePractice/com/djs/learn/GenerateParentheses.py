4f6275d6f55ddd0189fd807d24293bcbea896bbb
djsilenceboy <djdarkguardian@gmail.com>
2018-04-02 00:28:01 +0800

    Daily commit.

--- Python_Test/PyCodePractice/com/djs/learn/GenerateParentheses.py
+++ Python_Test/PyCodePractice/com/djs/learn/GenerateParentheses.py
@@ -6,16 +6,34 @@ Created on Mar 29, 2018
 
 
 class Solution:
+    def process(self, n):
+        if n == 1:
+            return ["()", ")("]
+        else:
+            sub_result = self.process(n - 1)
+            return sorted(list({"(" + x + ")" for x in sub_result} | {"()" + x for x in sub_result} | {x + "()" for x in sub_result}))
+
+    def generateParenthesis_2(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = self.process(n)
+        for item in result:
+            if item.startswith(")") or item.endswith("("):
+                result.remove(item)
+        return result
+
     def generateParenthesis(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
         if n == 1:
-            return ["()"]
+            return ["()", ")("]
         else:
             sub_result = self.generateParenthesis(n - 1)
-            return ["(" + x + ")" for x in sub_result] + ["()" + x for x in sub_result] + [x + "()" for x in sub_result]
+            return sorted(list({"(" + x + ")" for x in sub_result} | {"()" + x for x in sub_result} | {x + "()" for x in sub_result}))
 
 
 def test(n):
@@ -27,10 +45,14 @@ def test(n):
 
 def main():
     n = 3
+    # ['((()))', '(()())', '(())()', '()(())', '()()()']
     test(n)
 
+    n = 4
+    # ['(((())))','((()()))','((())())','((()))()','(()(()))','(()()())','(()())()',           '(())()()','()((()))','()(()())','()(())()','()()(())','()()()()']
+    # ["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]
+    test(n)
 
-print("-" * 80)
 
 if __name__ == '__main__':
     main()
