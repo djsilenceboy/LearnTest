a7c05333a491ff439723ef549761be063ca34ba2
djsilenceboy <djdarkguardian@gmail.com>
2019-01-02 23:37:11 +0800

    Daily commit.

--- Java_Test/WsConcurrentTaskController/src/main/java/com/djs/learn/ctc/ConcurrentTaskController.java
+++ Java_Test/WsConcurrentTaskController/src/main/java/com/djs/learn/ctc/ConcurrentTaskController.java
@@ -1,6 +1,7 @@
 
 package com.djs.learn.ctc;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,10 @@ public class ConcurrentTaskController
 
 	public void stop(){
 
+	}
+
+	@RequestMapping("/")
+	public String mainEntry(){
+		return "Hello World!";
 	}
 }
