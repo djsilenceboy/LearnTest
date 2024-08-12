982dace1b9c1957e967e3bde6b865ba964a15907
djsilenceboy <djdarkguardian@gmail.com>
2024-08-12 23:14:43 +0800

    Daily commit.

--- DataMining_Test/Property/URA/URA_ProcessedCsvData_Filter.sh
+++ DataMining_Test/Property/URA/URA_ProcessedCsvData_Filter.sh
@@ -7,8 +7,11 @@ CurrentFolder=$(pwd)
 DateRange="${FromDate}_${ToDate}"
 ProcessedCsvFileFolder="${CurrentFolder}/ProcessedCsvData/${DateRange}"
 
-TransPriceSummaryFileName="${ProcessedCsvFileFolder}/URA_CondoEcResults_${DateRange}_PriceSummary.csv"
-TransPriceSummaryFilteredFileName="${ProcessedCsvFileFolder}/URA_CondoEcResults_${DateRange}_PriceSummary_F.csv"
+PriceSummaryFileName="${ProcessedCsvFileFolder}/URA_CondoEcResults_${DateRange}_PriceSummary.csv"
+PriceSummaryFilteredFileName="${ProcessedCsvFileFolder}/URA_CondoEcResults_${DateRange}_PriceSummary_F.csv"
+
+PriceRentRatioFileName="${ProcessedCsvFileFolder}/URA_CondoEcResults_${DateRange}_PriceRentRatio.csv"
+PriceRentRatioFilteredFileName="${ProcessedCsvFileFolder}/URA_CondoEcResults_${DateRange}_PriceRentRatio_F.csv"
 
 echo "================================================================================"
 echo "Filter processed CSV data ..."
@@ -19,32 +22,73 @@ echo "FromDate = "$FromDate
 echo "ToDate = "$ToDate
 echo "DateRange = "$DateRange
 echo "ProcessedCsvFileFolder = "$ProcessedCsvFileFolder
-echo "TransPriceSummaryFileName = "$TransPriceSummaryFileName
-echo "TransPriceSummaryFilteredFileName = "$TransPriceSummaryFilteredFileName
+echo "PriceSummaryFileName = "$PriceSummaryFileName
+echo "PriceSummaryFilteredFileName = "$PriceSummaryFilteredFileName
+echo "PriceRentRatioFileName = "$PriceRentRatioFileName
+echo "PriceRentRatioFilteredFileName = "$PriceRentRatioFilteredFileName
 echo
 
-grep -h 'POSTAL_DISTRICT' $TransPriceSummaryFileName > $TransPriceSummaryFilteredFileName
+grep -h 'POSTAL_DISTRICT' $PriceSummaryFileName > $PriceSummaryFilteredFileName
+grep -h 'POSTAL_DISTRICT' $PriceRentRatioFileName > $PriceRentRatioFilteredFileName
 
 condoNameList=(
+",Astoria Park,"
 ",Casablanca,"
+",Caspian,"
 ",Central View,"
-",EastPoint Green,"
+",Chiltern Park,"
+",Coco Palms,"
+",Compass Heights,"
+",d'Nest,"
+",Double Bay Residences,"
+",Eastpoint Green,"
+",Eastvale,"
+",Esparina Residences,"
+",Heritage View,"
+",Kerrisdale,"
+",Kovan Melody,"
+",Kovan Residences,"
+",Livia,"
 ",Maysprings,"
+",Melville Park,"
+",Mi Casa,"
+",Millage,"
 ",Northoaks,"
 ",Northvale,"
+",Oleander Towers,"
 ",Orchid Park Condominium,"
 ",Palm Gardens,"
+",Parc Oasis,"
 ",Parc Vista,"
+",Prive,"
+",Rafflesia Condominium,"
 ",Regent Grove,"
 ",Rivervale Crest,"
 ",Rosewood,"
-",Simei Green,"
-",The Eden At Tampines,"
+",Sea Horizon,"
+",Simei Green Condominium,"
+",Stratum,"
+",Summerdale,"
+",Sun Plaza,"
+",Sunglade,"
+",Tanamera Crest,"
+",The Centris,"
+",The Dew,"
+",The Eden at Tampines,"
 ",The Esparis,"
-",The MayFair,"
+",The Estuary,"
+",The Jade,"
+",The Lakeshore,"
+",The Mayfair,"
+",The Minton,"
+",The Nautical,"
+",The Quartz,"
 ",The Warren,"
+",The Woodgrove,"
+",Watercolours,"
+",White Water,"
 ",Windermere,"
-",Woodgrove Condo,"
+",Woodgrove Condominium,"
 ",Woodsvale,"
 ",Yew Mei Green,"
 )
@@ -52,7 +96,8 @@ condoNameList=(
 for condoName in "${condoNameList[@]}"
 do
 	echo $condoName
-    grep -ih "$condoName" $TransPriceSummaryFileName >> $TransPriceSummaryFilteredFileName
+    grep -ih "$condoName" $PriceSummaryFileName >> $PriceSummaryFilteredFileName
+    grep -ih "$condoName" $PriceRentRatioFileName >> $PriceRentRatioFilteredFileName
 done
 
 echo
